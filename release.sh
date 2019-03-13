426208afb304c6198483b3bd93886f920190d50f
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2019-03-13 15:10:37 -0500

    update release.sh

--- release.sh
+++ release.sh
@@ -91,7 +91,26 @@ then
             }
         ]
     }' 'https://hooks.slack.com/services/'$SLACKPARAM1'/'$SLACKPARAM2'/'$SLACKPARAM3
-    exit 0
+
+    #EMAIL
+    # Just the git commit message title
+    title=$(git log -1 --pretty=%B | head -n 1)
+    git checkout master
+    # Save the messages into an array called message
+    IFS=$'\n' message=($(git log -1 --pretty=%B | sed -e '1,2d'))
+
+    if [[ $title = *"SENDEMAIL"* ]];
+    then
+      formattedmessages=''
+      for i in "${message[@]}"
+      do
+        formattedmessages=$formattedmessages'|'$i
+      done
+
+      json='{"authenticationPassword":"'$EMAILAUTHPASS'", "messages" : "'$formattedmessages'", "packageManaged": false, "instructions": "https://github.com/voiceittech/VoiceIt2-Cpp/releases/download/'$wrapperplatformversion'/VoiceIt2.hpp"}'
+      curl -X POST -H "Content-Type: application/json" -d $json "https://api.voiceit.io/platform/34"
+    fi
+
   else
     curl -X POST -H 'Content-type: application/json' --data '{
       "icon_url": "https://s3.amazonaws.com/voiceit-api2-testing-files/test-data/TravisCI-Mascot-1.png",
@@ -107,3 +126,5 @@ then
     exit 1
   fi
 fi
+
+
