02aeb532096f55967edf0ef8f0069c7c1df57606
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2019-04-03 14:52:18 -0500

    small update

--- Test.cpp
+++ Test.cpp
@@ -68,18 +68,20 @@ void DownloadFile(std::string destination, std::string source)
 
 }
 
-int main()
+int main(int argc, char *argv[])
 {
 #ifndef _WIN32
-	VoiceIt2 v(getenv("VIAPIKEY"), getenv("VIAPITOKEN"));
-	std::string boxfuseenv = getenv("BOXFUSE_ENV");
-	if (boxfuseenv == "voiceittest")
-  {
-		std::string home = getenv("HOME");
-		std::ofstream out(home + "/platformVersion");
-		out << v.GetVersion();
-		out.close();
-	}
+	VoiceIt2 v(argv[1], argv[2]);
+  if (getenv("BOXFUSE_ENV") != NULL) {
+    std::string boxfuseenv = getenv("BOXFUSE_ENV");
+    if (boxfuseenv == "voiceittest")
+    {
+      std::string home = getenv("HOME");
+      std::ofstream out(home + "/platformVersion");
+      out << v.GetVersion();
+      out.close();
+    }
+  }
 #else
 	VoiceIt2 v("key_00000000000000000000000000000000", "tok_00000000000000000000000000000000");
 #endif
